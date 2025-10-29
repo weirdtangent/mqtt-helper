35a4cbf81739b6b90b572c840a7a6577da1c85c8
Jeff Culverhouse <jeff@graystorm.com>
2025-10-28 21:19:49 -0600

    fix: auto-include service_slug in discovery topic item

--- src/mqtt_helper/core.py
+++ src/mqtt_helper/core.py
@@ -38,7 +38,7 @@ class MqttHelper:
         return "/".join([self.service_slug, component_type, self.device_slug(device_id), *map(str, parts)])
 
     def disc_t(self, component: str, item: str) -> str:
-        return "/".join(["homeassistant",component, item, "config"])
+        return "/".join(["homeassistant",component, f"{self.service_slug}_{item}", "config"])
 
     def stat_t(self, device_id: str, category: str, *parts: str) -> str:
         if device_id == "service":
