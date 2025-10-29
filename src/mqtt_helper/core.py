b3203b999d4df8711dd5fbb883d34b19c9e78590
Jeff Culverhouse <jeff@graystorm.com>
2025-10-29 04:24:46 -0600

    chore: fix logger; config ruff/black

--- src/mqtt_helper/core.py
+++ src/mqtt_helper/core.py
@@ -12,7 +12,7 @@ class MqttHelper:
         self.service = service
         self.service_slug = re.sub(r"[^a-zA-Z0-9]+", "", service)
 
-    # MQTT connections ----------------------------------------------------------------------------
+    # IDs -----------------------------------------------------------------------------------------
 
     def client_id(self) -> str:
         return "-".join(
@@ -22,22 +22,16 @@ class MqttHelper:
             ]
         )
 
-    # IDs -----------------------------------------------------------------------------------------
-
     def svc_unique_id(self, entity: str = "") -> str:
         return "_".join([self.service_slug, re.sub(r"[^a-zA-Z0-9]+", "", entity)])
 
     def dev_unique_id(self, device_id: str, entity: str = "") -> str:
-        return "_".join(
-            [self.device_slug(device_id), re.sub(r"[^a-zA-Z0-9]+", "", entity)]
-        )
+        return "_".join([self.device_slug(device_id), re.sub(r"[^a-zA-Z0-9]+", "", entity)])
 
     # Slug strings --------------------------------------------------------------------------------
 
     def device_slug(self, device_id: str) -> str:
-        return "_".join(
-            filter(None, [self.service_slug, re.sub(r"[^a-zA-Z0-9]+", "", device_id)])
-        )
+        return "_".join(filter(None, [self.service_slug, re.sub(r"[^a-zA-Z0-9]+", "", device_id)]))
 
     # Topic strings -------------------------------------------------------------------------------
 
@@ -59,32 +53,22 @@ class MqttHelper:
     def disc_t(self, component: str, item: str) -> str:
         if not component or not item:
             raise ValueError("[disc_t] component and item have to be non-blank")
-        return "/".join(
-            ["homeassistant", component, self.service_slug + "_" + item, "config"]
-        )
+        return "/".join(["homeassistant", component, self.service_slug + "_" + item, "config"])
 
     def stat_t(self, device_id: str, category: str, *parts: str) -> str:
         if device_id == "service":
             return "/".join([self.service_slug, category, *map(str, parts)])
-        return "/".join(
-            [self.service_slug, self.device_slug(device_id), category, *map(str, parts)]
-        )
+        return "/".join([self.service_slug, self.device_slug(device_id), category, *map(str, parts)])
 
-    def avty_t(
-        self, device_id: str, category: str = "availability", *parts: str
-    ) -> str:
+    def avty_t(self, device_id: str, category: str = "availability", *parts: str) -> str:
         if device_id == "service":
             return "/".join([self.service_slug, category, *map(str, parts)])
-        return "/".join(
-            [self.service_slug, self.device_slug(device_id), category, *map(str, parts)]
-        )
+        return "/".join([self.service_slug, self.device_slug(device_id), category, *map(str, parts)])
 
     def attr_t(self, device_id: str, category: str = "attributes", *parts: str) -> str:
         if device_id == "service":
             return "/".join([self.service_slug, category, *map(str, parts)])
-        return "/".join(
-            ["homeassistant", self.device_slug(device_id), category, *map(str, parts)]
-        )
+        return "/".join(["homeassistant", self.device_slug(device_id), category, *map(str, parts)])
 
     def cmd_t(self, device_id: str, category: str = "cmd", *parts: str) -> str:
         if device_id == "service":
@@ -101,14 +85,7 @@ class MqttHelper:
 
     # Misc helpers --------------------------------------------------------------------------------
 
-    def device_block(
-        self,
-        name: str,
-        id: str,
-        mfr: str,
-        device_version: str = "",
-        service_version: str = "",
-    ) -> dict[str, Sequence[str]]:
+    def device_block(self, name: str, id: str, mfr: str, device_version: str = "", service_version: str = "") -> dict[str, Sequence[str]]:
         device = {"name": name, "identifiers": [id], "manufacturer": mfr}
 
         if device_version:
@@ -123,28 +100,18 @@ class MqttHelper:
             )
         return device
 
-    def safe_publish(
-        self, topic: str, payload: str | bool | int | dict | None, **kwargs: Any
-    ) -> None:
+    def safe_publish(self, topic: str, payload: str | bool | int | dict | None, **kwargs: Any) -> None:
         if not topic:
             raise ValueError("Cannot post to a blank topic")
-        if isinstance(payload, dict) and (
-            "component" in payload or "//////" in payload
-        ):
-            self.logger.warning(
-                "Questionable payload includes 'component' or string of slashes - wont't send to HA"
-            )
+        if isinstance(payload, dict) and ("component" in payload or "//////" in payload):
+            self.logger.warning("Questionable payload includes 'component' or string of slashes - wont't send to HA")
             self.logger.warning(f"topic: {topic}")
             self.logger.warning(f"payload: {payload}")
-            raise ValueError(
-                "Possible invalid payload. topic: {topic} payload: {payload}"
-            )
+            raise ValueError("Possible invalid payload. topic: {topic} payload: {payload}")
         try:
             if payload is None:
                 self.mqttc.publish(topic, "null", **kwargs)
             else:
                 self.mqttc.publish(topic, cast(PayloadType, payload), **kwargs)
         except Exception as e:
-            self.logger.warning(
-                f"MQTT publish failed for {topic} with {payload[:120] if isinstance(payload, str) else payload}: {e}"
-            )
+            self.logger.warning(f"MQTT publish failed for {topic} with {payload[:120] if isinstance(payload, str) else payload}: {e}")
